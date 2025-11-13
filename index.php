45153c3e56c65c4f1877e8118c5054e551648730
robiya2011 <abduvaliyevasolihabonu665@gmail.com>
2025-11-13 16:23:03 +0500

    birinchi commit1

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-widht, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-$a = 2;
-if ($a >-1) {
-echo $a**2;
-}
-else{
-    echo $a;
-}
+ $x=10;
+  $y=5;
+ $yigindi=$x+$y;
+ $ayirma=$x-$y;
+ $kopaytma=$x*$y;
+$bolinma=$x/$y;
+echo " yig'indi= $yigindi" ."<br>";
+echo  " ayirma=$ayirma" ."<br>";
+echo  "ko'paytma=$kopaytma". "<br>";
+echo  " bo'linma= $bolinma" ."<br>";
 ?>
+</body>
+</html>
