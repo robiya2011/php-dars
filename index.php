c5d1e4f5baf2e3ee9eeebd99463c9f0b0581d88d
robiya2011 <abduvaliyevasolihabonu665@gmail.com>
2025-11-22 16:49:15 +0500

    if:else bayonot

--- index.php
+++ index.php
@@ -1,39 +1,20 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-widht, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
- $a=10;
- switch ($a){
-    case  "1";
-    echo " Bugun dushanba !";
-    break;
-    case "2";
-    echo " Bugun seyshanba !";
-    break;
-    case "3";
-    echo " Bugun chorshanba";
-    break;
-    case "4";
-    echo "Bugun payshanba !";
-    break;
-    case  "5";
-    echo " Bugun juma !";
-    break;
-    case "6";
-    echo "Bugun sanba !";
-    break;
-    case "7";
-    echo " Bugun yakshanba !";
-    break;
-    default;
-    echo "Bunday xafta kun yo'q !";
- }
+$x=10;
+if( $x>0 && $x<=10){
+   echo $x**3;
+}
+
+   elseif( $x>10 && $x<50){
+    echo sqrt($x);
+   }
+else{
+    echo "Bunday natija yo'q";
+}
+
+
+
+
+
+
+
 ?>
-</body>
-</html>
